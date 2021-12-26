7c0a106d5c79c429909a3c11769b93da1105a891
izvanzemaljci <sanja.milovic@stu.ssst.edu.ba>
2021-12-26 11:57:16 +0100

    Created a character controller script

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    private Collider unitCollider;
+
+    private void Update()
+    {
+        MoveToUnit();
+
+        if (!Input.GetMouseButtonDown(0))
+        {
+            return;
+        }
+
+        GetUnitCenter();
+    }
+
+    private void GetUnitCenter()
+    {
+        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hitInfo);
+
+        if (hit && hitInfo.transform.CompareTag("Unit"))
+        {
+            unitCollider = hitInfo.collider;
+        }
+    }
+
+    private void MoveToUnit()
+    {
+        if (!unitCollider)
+        {
+            return;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, unitCollider.bounds.center, movementSpeed * Time.deltaTime);
+    }
+}
