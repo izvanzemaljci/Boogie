a1380cc4f5668339d51955c14a7570e988bd2dc2
izvanzemaljci <sanja.milovic@stu.ssst.edu.ba>
2021-12-26 12:01:02 +0100

    Script cleanup

--- Assets/Scripts/Podium/UnitController.cs
+++ Assets/Scripts/Podium/UnitController.cs
@@ -6,15 +6,32 @@ public class UnitController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (!Input.GetMouseButtonDown(0))
         {
-            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hitInfo);
-
-            if(hit && hitInfo.transform.CompareTag("Unit"))
-            {
-                lastHitUnit = hitInfo.transform.gameObject.GetComponent<Unit>();
-                lastHitUnit.UnitLit();
-            }
+            return;
         }
+
+        GetLastHitUnit();
+    }
+
+    private void GetLastHitUnit()
+    {
+        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hitInfo);
+
+        if (hit && hitInfo.transform.CompareTag("Unit"))
+        {
+            lastHitUnit = hitInfo.transform.gameObject.GetComponent<Unit>();
+            OnUnitHit();
+        }
+    }
+
+    private void OnUnitHit()
+    {
+        if(!lastHitUnit)
+        {
+            return;
+        }
+
+        lastHitUnit.UnitLit();
     }
 }
